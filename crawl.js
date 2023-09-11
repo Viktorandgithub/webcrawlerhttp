14d530e78a3b3df526d50c6eede27f4ce30f3d6d
Viktorandgithub <97830751+Viktorandgithub@users.noreply.github.com>
2023-09-11 19:18:18 +0200

    crawl page recursive

--- crawl.js
+++ crawl.js
@@ -1,6 +1,22 @@
 const { JSDOM } = require("jsdom");
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+  const baseURLObj = new URL(baseURL);
+  const currnetURLObj = new URL(currentURL);
+
+  if (baseURLObj.hostname !== currnetURLObj.hostname) {
+    return pages;
+  }
+
+  const normalizedCurrentURL = normalizeURL(currentURL);
+
+  if (pages[normalizedCurrentURL] > 0) {
+    pages[normalizedCurrentURL]++;
+    return pages;
+  }
+
+  pages[normalizedCurrentURL] = 1;
+
   console.log(`actively crawling: ${currentURL}`);
 
   try {
@@ -10,18 +26,27 @@ async function crawlPage(currentURL) {
       console.log(
         `error in fetch: ${err.message}. with statucode  ${resp.status} on page ${currentURL}`
       );
-      return;
+      return pages;
     }
     const contentType = resp.headers.get("content-type");
 
     if (!contentType.includes("text/html")) {
       console.log(`non html response: ${err.message}. on page ${currentURL}`);
-      return;
+      return pages;
     }
-    console.log(await resp.text());
+
+    const htmlBody = await resp.text();
+
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL);
+
+    for (const nextURL of nextURLs) {
+      pages = await crawlPage(baseURL, nextURL, pages);
+    }
   } catch (err) {
     console.log(`error in fetch: ${err.message}. on page ${currentURL}`);
   }
+
+  return pages;
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
