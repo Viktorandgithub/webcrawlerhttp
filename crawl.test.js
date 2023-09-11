9267f01413648976a032ba0a0401752d093d2035
Viktorandgithub <97830751+Viktorandgithub@users.noreply.github.com>
2023-09-11 18:34:37 +0200

    getURLsFromHTML & test

--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require("./crawl.js");
+const { normalizeURL, getURLsFromHTML } = require("./crawl.js");
 const { test, expect } = require("@jest/globals");
 
 test("normalizeURL strip protocol", () => {
@@ -22,9 +22,79 @@ test("normalizeURL capitals", () => {
   expect(actual).toEqual(expected);
 });
 
-test("normalizeURL http strip", () => {
+test("normalizeURL slash", () => {
   const input = "http://youtube.com/path/";
   const actual = normalizeURL(input);
   const expected = "youtube.com/path";
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML http strip", () => {
+  const htmlBodyInput = `<html>
+  <body>
+  <a href="https://youtube.com/path">
+  Youtube.com 
+  </a>
+  </body>
+  </html>`;
+  const baseURLInput = "https://youtube.com/path";
+  const actual = getURLsFromHTML(htmlBodyInput, baseURLInput);
+  const expected = ["https://youtube.com/path"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML http strip relativ url", () => {
+  const htmlBodyInput = `<html>
+    <body>
+    <a href="/path/">
+    Youtube.com 
+    </a>
+    </body>
+    </html>`;
+  const baseURLInput = "https://youtube.com";
+  const actual = getURLsFromHTML(htmlBodyInput, baseURLInput);
+  const expected = ["https://youtube.com/path/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML multiple urls", () => {
+  const htmlBodyInput = `<html>
+      <body>
+      <a href="/path/">
+      Youtube.com 
+      </a>
+      <a href="/slash/">
+      Youtube.com 
+      </a>
+      <a href="https://youtube.com/path1">
+      Youtube.com 
+      </a>
+      <a href="https://youtube.com/path2">
+        Youtube.com 
+        </a>
+      </body>
+      </html>`;
+  const baseURLInput = "https://youtube.com";
+  const actual = getURLsFromHTML(htmlBodyInput, baseURLInput);
+  const expected = [
+    "https://youtube.com/path/",
+    "https://youtube.com/slash/",
+    "https://youtube.com/path1",
+    "https://youtube.com/path2",
+  ];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML invalid url", () => {
+  const htmlBodyInput = `<html>
+      <body>
+      <a href="invalid">
+      Youtube.com 
+      </a>
+      </body>
+      </html>`;
+  const baseURLInput = "https://youtube.com";
+  const actual = getURLsFromHTML(htmlBodyInput, baseURLInput);
+  const expected = [];
   expect(actual).toEqual(expected);
 });
